f9ba6a541658b610e7eb2a5f6c1036ad7e8ab55e
Anna Ruiz Verdaguer <anna.ruizverdaguer@gmail.com>
2023-09-10 20:46:18 +0200

    Preparing tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      "broken-white": "#FAF7F2",
+      "primary-color": "#2BB3C0",
+      "secondary-color": "#EF6C35",
+      "fake-black": "#161C2E"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
