8af572e5e283886ab9c75c7e6a1332e1928d2f61
Anna Ruiz Verdaguer <anna.ruizverdaguer@gmail.com>
2023-09-11 13:22:15 +0200

    Adding the item component + minor refactor on Item type name

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-import { Item } from "./types/types";
+import { ItemType } from "./types/types";
 
 export const ITEMS_API = "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/items.json"
-export const ITEM_FILTER_PARAMS = ['title' as keyof Item, 'description' as keyof Item, 'price' as keyof Item, 'email' as keyof Item];
+export const ITEM_FILTER_PARAMS = ['title' as keyof ItemType, 'description' as keyof ItemType, 'price' as keyof ItemType, 'email' as keyof ItemType];
