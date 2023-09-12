141f7f4233c84357403ec0c6f01b318dc01afbdf
Anna Ruiz Verdaguer <anna.ruizverdaguer@gmail.com>
2023-09-12 18:34:51 +0200

    adding the logic to be able to print a favorite's list

--- src/item/Item.tsx
+++ src/item/Item.tsx
@@ -1,13 +1,36 @@
 import { ItemType } from '../types/types';
+import { useContext, useState } from 'react';
+import { FavoritesItemsContext } from '@/contexts/FavoritesItemsContext';
 
-export default function Item({ title, description, price, email, image }: ItemType) {
+type itemProps = {
+    item: ItemType,
+    favorite: boolean
+}
+
+export default function Item({ item, favorite }: itemProps) {
+    const favoriteItems = useContext(FavoritesItemsContext)
+
+    function addFav() {
+        let favoriteItemsCopy = favoriteItems.items;
+        favoriteItemsCopy.push(item)
+        favoriteItems.setItems(favoriteItemsCopy);
+    }
+
+    function removeFav() {
+        let favoriteItemsCopy = favoriteItems.items;
+        let index = favoriteItemsCopy.indexOf(item);
+        favoriteItemsCopy.splice(index, 1);
+        favoriteItems.setItems(favoriteItemsCopy);
+    }
+
     return (
         <div className="item-card">
-            <img src={image} alt={title} className="item-image" />
-            <h3 className='item-title'>{title}</h3>
-            <h4 className='item-price'>{price}€</h4>
-            <p className='item-description'>{description}</p>
-            <p className='item-email'>{email}</p>
+            <img src={item.image} alt={item.title} className="item-image" />
+            <h3 className='item-title'>{item.title}</h3>
+            {favorite ? <button className='item-favorite' onClick={removeFav}>💖</button> : <button className='item-favorite' onClick={addFav}>🖤</button>}
+            <h4 className='item-price'>{item.price}€</h4>
+            <p className='item-description'>{item.description}</p>
+            <p className='item-email'>{item.email}</p>
         </div>
     )
 }
