6abd52eaa57ee6deffaf8e268d9e853d2f113e83
Anna Ruiz Verdaguer <anna.ruizverdaguer@gmail.com>
2023-09-15 19:35:40 +0200

    adding the favorites items list in the modal + some minor improvements on itemList

--- src/itemList/ItemList.tsx
+++ src/itemList/ItemList.tsx
@@ -1,13 +1,9 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import Item from '../item/Item';
 import { FilteredItemsContext } from '@/contexts/FilteredItemsContext';
 
 export default function ItemList() {
     const filteredItems = useContext(FilteredItemsContext)
-
-    useEffect(() => {
-        filteredItems.items.map((item) => <Item title={item.title} description={item.description} price={item.price} email={item.email} image={item.image} fav={item.fav} />)
-    }, [filteredItems])
 
     return (
         <div className="item-list">
--- src/modal/Modal.tsx
+++ src/modal/Modal.tsx
@@ -1,21 +1,30 @@
+import { useContext } from 'react';
+import { ItemsContext } from '../contexts/ItemsContext';
+import FavoriteItem from '@/favoriteItem/FavoriteItem';
+
 interface IModalProps {
     isOpen: boolean;
     onModalClose(data: object): void;
 }
 
 export default function Modal({ isOpen = false, onModalClose }: IModalProps) {
+    const items = useContext(ItemsContext)
 
     if (!isOpen) {
         return null;
     }
     document.body.style.overflow = 'hidden';
+    const favItemsList = items.items.filter(item => { return item.fav === true });
 
     return (
         <div className="modal-outside-background">
             <div className="modal-inside-background">
                 <div className='modal-header-section'>
-                    <h2 className='modal-title'>Modal content</h2>
+                    <h2 className='modal-title'>Your favorite items:</h2>
                     <button className="modal-close-button" onClick={onModalClose}>X</button>
+                </div>
+                <div>
+                    {favItemsList.map((item) => <FavoriteItem title={item.title} image={item.image} />)}
                 </div>
             </div>
         </div>
