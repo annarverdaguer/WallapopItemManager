59e0f0be9de2e10279737c5d5fe02bc1e040132c
Anna Ruiz Verdaguer <anna.ruizverdaguer@gmail.com>
2023-09-15 09:25:52 +0200

    bug fixing & simplifying some logics

--- src/contexts/FavoritesItemsContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { ItemContextType } from "../types/types";
-
-const FavoritesItemContextDefaults: ItemContextType = {
-    items: [],
-    setItems: () => { }
-}
-
-export const FavoritesItemsContext = createContext(FavoritesItemContextDefaults);
