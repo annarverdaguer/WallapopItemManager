1df36cc8e83ddc8c6c8f6e5a4a7efb4cc4f96b86
Anna Ruiz Verdaguer <anna.ruizverdaguer@gmail.com>
2023-09-17 19:35:29 +0200

    Adding tests for the Search component

--- src/pagination/Paginate.test.tsx
+++ src/pagination/Paginate.test.tsx
@@ -1,5 +1,5 @@
 import Paginate, { IPaginationProps } from "./Paginate"
-import { fireEvent, render, RenderResult } from "@testing-library/react"
+import { render, RenderResult } from "@testing-library/react"
 
 describe("<Paginate />", () => {
     let pagination: RenderResult<typeof import("@testing-library/dom/types/queries"), HTMLElement>;
@@ -11,7 +11,7 @@ describe("<Paginate />", () => {
         pagination = render(<Paginate postsPerPage={props.postsPerPage} totalPosts={props.totalPosts} paginate={props.paginate} currentPage={props.currentPage} />);
     })
 
-    test('Verifying pagination is rendered', async () => {
+    test('Verifying pagination is rendered', () => {
         const firstPage = pagination.getByText(/1/);
         expect(firstPage).toBeDefined();
     })
