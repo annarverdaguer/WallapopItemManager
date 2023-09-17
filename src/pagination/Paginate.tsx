fc6db3cd457f6455898e7df35f382dafae917c62
Anna Ruiz Verdaguer <anna.ruizverdaguer@gmail.com>
2023-09-17 12:42:39 +0200

    adding pagination + other style fixes

---
+++ src/pagination/Paginate.tsx
@@ -0,0 +1,27 @@
+
+interface IPaginationProps {
+    postsPerPage: number;
+    totalPosts: number;
+    paginate: Function;
+    currentPage: number;
+}
+
+export default function Paginate({ postsPerPage, totalPosts, paginate, currentPage }: IPaginationProps) {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <div className="pagination-container">
+            <ul className="pagination">
+                {pageNumbers.map((number) => (
+                    <button key={number} className={`pagination-page-number ${currentPage === number ? 'button-active' : ''}`} onClick={() => paginate(number)}>
+                        {number}
+                    </button>
+                ))}
+            </ul>
+        </div>
+    );
+};
